cffc567942f487ce07622d06dc5430ae24f91b30
PredragWebDev <pedjolinodev@gmail.com>
2023-10-20 22:41:32 +0900

    get list from txt and implement

--- server/Controllers/LinksController.cs
+++ server/Controllers/LinksController.cs
@@ -26,7 +26,6 @@ using Abot2.Core;
 using Abot2.Crawler;
 using Abot2.Poco;
 using Serilog;
-using System.IO.FileSystem
 
 namespace server.Controllers;
 
@@ -40,11 +39,6 @@ public class LinkCrawler
 
         var URI = "";
 
-        if (IgnoreDomain(domain)) {
-            Console.WriteLine("ignore>>>.");
-            return links;
-        }
-
         if (!crawler.IsCorrectURI(domain?? "")) {
 
             URI = ConvertToURI(domain ?? "");
@@ -52,8 +46,6 @@ public class LinkCrawler
         else {
             URI = domain;
         }
-
-        // string url = $"http://{domain}"; // Create the URL to crawl
 
         Console.WriteLine($"url>>>> {URI}");
 
@@ -67,13 +59,7 @@ public class LinkCrawler
             {
                 string link = WebUtility.HtmlDecode(linkNode.GetAttributeValue("href", ""));
 
-                if (Check_link(link, domain ?? ""))
-                {
-                    if (!Check_existing(links, link))
-                    {
-                        links.Add(PickDomainFromURL(link));
-                    }
-                }
+                links.Add(PickDomainFromURL(link));
             }
 
         }
@@ -83,9 +69,18 @@ public class LinkCrawler
             // Handle any other exceptions that might occur during the execution of the code
         }
 
-
         return links;
         
+    }
+
+    public string[] Get_Lists() {
+
+        Console.WriteLine("okay?");
+        var lists = System.IO.File.ReadAllLines("link lists.txt");
+
+        Console.WriteLine($"lists>>>: {lists}");
+
+        return lists;
     }
 
     public bool IsCorrectURI(string domain)
@@ -101,28 +96,6 @@ public class LinkCrawler
         string URI = $"http://{domain}";
         
         return URI;
-    }
-
-    private bool IgnoreDomain(string domain) {
-        List<string> IgnoreDomain = new (
-            new []
-            {
-            "facebook.com",
-            "linkedin.com",
-            "twitter.com",
-            "youtube.com",
-            "skype.com",
-            "instagram.com",
-            "discord.com",
-            "slack.com",
-            "whatsapp.com",}
-        );
-
-        if (IgnoreDomain.Contains(domain)) {
-            return true;
-        }
-
-        return false;
     }
 
     public string PickDomainFromURL(string URL) {
@@ -184,37 +157,26 @@ public class LinksController : ControllerBase
         };
 
         var crawler = new LinkCrawler();
-        var index = 0;
-        do {
-            List<string> templinks  = crawler.CrawlLinks(links[index]);
-            foreach (var link in templinks) {
-                if (!crawler.Check_existing(links, link)) {
-                    links.Add(link);
+
+        string[] link_lists = crawler.Get_Lists();
+
+        foreach (var link in link_lists) {
+
+            List<string> templinks  = crawler.CrawlLinks(link);
+            foreach (var templink in templinks) {
+
+                if (crawler.Check_link(templink, domain)) {
+                    string temp = crawler.PickDomainFromURL(templink);
+
+                    if (!crawler.Check_existing(links, domain)) {
+
+                        Console.WriteLine($"added link>>>> {templink}");
+                        links.Add(templink);
+                    }
                 }
             }
 
-            index ++;
-        } while(links.Count > index && links.Count < 1000);
-        // List<string> links =  crawler.CrawlLinks(domain ?? "");
-        //////////////////////////////////////////////////////////////////////////////////////////////////
-        // string apiKey = "AIzaSyCqY41oLU4KL9JBIPsZyFF4W9A00WmlKsI";
-        // string apiKey = "AIzaSyDK_BNn-W6zDYg4D1Jy-0mMQvR-hHDJTPA";
-        // string apiKey = "AIzaSyAWp2AFSPxOxcPeJGB6iddHqHwPayphJDg";
-        // string searchEngineId = "d61f36940c5cf411e";
-        // string searchEngineId = "7559c0c631de64c8a";
-        // string searchEngineId = "e6b0fb4939f2c4b50";
-        // var backlinkService = new BacklinkService();
-        // List<string> links = await backlinkService.GetBacklinks10( domain, searchEngineId, apiKey);
-        // List<string> links = await backlinkService.GetBacklinks( domain ?? "", searchEngineId, apiKey);
-
-        //////////////////////////////////////////////////////////////////////////////////////////////////    
-        
-        // var testAbotUse = new TestAbotUse();
-
-        // await TestAbotUse.DemoSimpleCrawler(domain ?? "");
-
-        // await TestAbotUse.DemoSinglePageRequest();
-
+        }
 
         if (links.Count > 0)
         {
@@ -225,10 +187,6 @@ public class LinksController : ControllerBase
 
         return await Task.FromResult(Ok(links));
         // return Ok(links);
-    }
-
-    public List<string> CrawlLinks(string domain) {
-
     }
 
     public async void Save_Backlink(List<string> links, string domain) {
