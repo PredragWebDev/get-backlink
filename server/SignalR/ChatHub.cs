985c7c380c95db6a5c4d6d71c92e33b2a0fe3750
PredragWebDev <pedjolinodev@gmail.com>
2023-10-24 18:12:51 +0900

    use scraping

--- server/SignalR/ChatHub.cs
+++ server/SignalR/ChatHub.cs
@@ -1,5 +1,8 @@
 using Microsoft.AspNetCore.SignalR;
 using crawler;
+using scraping;
+using GoogleSearch;
+
 namespace SignalR
 {
     public class ChatHub : Hub
@@ -9,53 +12,65 @@ namespace SignalR
             LinkCrawler crawler = new();
             List<string> links =  new();
             List<DateTime> date_time = new();
+            ////////////////////////////////////////////
+            // LinkScraping scraper = new();
 
-            string[] link_lists = crawler.Get_Lists();
+            // List<string> link_lists = scraper.GetBacklinks(domain);
 
-            var number_of_list = link_lists.Length;
+            // foreach (string backlink in link_lists)
+            // {
+            //     Console.WriteLine(backlink);
+            // }
+            //////////////////////////////////////////
+            // string apiKey = "AIzaSyCqY41oLU4KL9JBIPsZyFF4W9A00WmlKsI";
+            string apiKey = "AIzaSyDK_BNn-W6zDYg4D1Jy-0mMQvR-hHDJTPA";
+            // string searchEngineId = "d61f36940c5cf411e";
+            string searchEngineId = "7559c0c631de64c8a";
+            GoogleSearch searchEngine = new GoogleSearch();
+            List<string> link_lists = searchEngine.GetBacklinks();
+
+            ///////////////////////////////////////
+
+            // string[] link_lists = crawler.Get_Lists();
+
+            var number_of_list = link_lists.Count;
             var index = 0;
 
             foreach (var link in link_lists) {
                 DateTime curTime = DateTime.Now;
                 
-                // await Clients.All.SendAsync("link", link, curTime);
                 index ++;
                 await Clients.Caller.SendAsync("progress_bar", (double)index/number_of_list * 100);
 
-                // links.Add(link);
-                // date_time.Add(curTime);
+                // List<string> templinks = new ();
+                // templinks = await crawler.get_Sublink_From_DB(link);
+                // if (templinks.Count < 1) {
 
-                // Console.WriteLine($"progress: {(int)Math.Ceiling((double)index/number_of_list * 100)} number of list: {number_of_list} index: {index}");
-                List<string> templinks = new ();
-                templinks = await crawler.get_Sublink_From_DB(link);
-                if (templinks.Count < 1) {
+                //     templinks  = crawler.CrawlLinks(link);
 
-                    templinks  = crawler.CrawlLinks(link);
+                //     crawler.Save_Sublink(templinks, link);
+                // }
 
-                    crawler.Save_Sublink(templinks, link);
-                }
+                // foreach (var templink in templinks) {
 
-                foreach (var templink in templinks) {
-
-                    // await Clients.All.SendAsync("link", templink, curTime);
-
-                    if (crawler.Check_link(templink, domain ?? "")) {
-                        
-                        Console.WriteLine($"link>>> {templink} domain>>>: {domain}");
+                    if (crawler.Check_link(link, domain ?? "")) {
+                        Console.WriteLine($"link>>> {link} domain>>>: {domain}");
 
                         // string temp = crawler.PickDomainFromURL(templink);
 
-                        if (!crawler.Check_existing(links, link)) {
+                        string str_picked_domain = crawler.PickDomainFromURL(link);
+
+                        if (!crawler.Check_existing(links, str_picked_domain)) {
                             DateTime cur_Time = DateTime.Now;
                             Console.WriteLine($"added link>>>> {link}");
                             await Clients.Caller.SendAsync("link", link, cur_Time);
-                            // await Clients.All.SendAsync("progress_bar", (int)Math.Ceiling((double)index/number_of_list * 100));
-                            links.Add(link);
+                            
+                            links.Add(str_picked_domain);
                             date_time.Add(cur_Time);
                             break;
                         }
                     }
-                }
+                // }
             }
 
              if (links.Count > 0)
